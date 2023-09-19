bb4cc6e8cf5816559050a51a689f73b41e437828
AlexDai54202 <jalexdai54202@gmail.com>
2023-09-19 16:46:25 -0400

    added unit tests, added verbose argument to big_run.py and uexp_to_raw.bat.

--- big_run.py
+++ big_run.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import os
 
+verbose = False
+
 def main() -> int:
     #print('Number of arguments:', len(sys.argv), 'arguments.')
     #print('Argument List:', str(sys.argv))
@@ -12,13 +14,17 @@ def main() -> int:
     while dirlist != []:
         dir = dirlist.pop()
         short_dir = dir.replace(os.getcwd(),'.')
-        print("DIRECTORY:", short_dir)
+        
+        if verbose:
+            print("DIRECTORY:", short_dir)
+        
         os.makedirs(f'.\\csv_outputs\\{short_dir[2:]}',exist_ok=True)
         files = os.listdir(dir)
         for f in files:
             dirf = dir +'\\' + f
             if f[-5:] == '.uexp':
-                print(str(cmd+[dirf]))
+                if verbose:
+                    print(str(cmd+[dirf]))
                 if (not os.path.isfile(f'.\\csv_outputs\\{short_dir[2:]}\\{f[:-5]}.csv')) or ('\\Event\\' not in dirf): # is Event, has duplicate: don't pass. 
                     p = subprocess.Popen(cmd+[dirf])
                     p.wait()
@@ -27,10 +33,11 @@ def main() -> int:
                     else:
                         failed_to_parse += [f'{short_dir[2:]}\\{f[:-5]}']
             if os.path.isdir(dirf):
-                print("***"+dirf)
+                if verbose:
+                    print("***"+dirf)
                 if 'parse_raw_bytes' not in dirf and 'csv_outputs' not in dirf:
                     dirlist.append(dirf)
-    print(failed_to_parse)
+    print('Failed to parse the following files: ' + str(failed_to_parse))
     return 0
 
 if __name__ == '__main__':
